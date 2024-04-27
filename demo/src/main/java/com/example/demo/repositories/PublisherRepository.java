a59bc96de722ec95cecd716a57f1fea42d578b0c
Jair Palacios <jairpalacios386@gmail.com>
2024-04-27 14:49:20 -0400

    Creats POJO and repository for Author, Book, and Publisher

---
+++ demo/src/main/java/com/example/demo/repositories/PublisherRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domain.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
+
